db2678026e6dac62e87c7952d43d10b5c60e2df8
Sol Park <vic2107@naver.com>
2023-01-17 12:15:29 +0900

    Third commit

--- Hello_World.c
+++ Hello_World.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main(){
+    int i;
     printf("First commit\n");
     printf("Second commit\n");
+    for (i=0;i<5;i++){
+        printf("ÈÖ¿¬ ¼±¹è ¿¬±Ø ¸ÚÀÖ´Ù\n"); 
+    }
     return 0;
 }
